66daf84bab9ffb68254aa9188130ba66a25b3d51
Richard Connor <>
2020-05-25 01:17:57 +0100

    new angular search structures, vpt and laesa. vpt won't work because of locality, probably

--- src/main/java/uk/al_richard/branch_git_experiment/GenerateAngleHistogram.java
+++ src/main/java/uk/al_richard/branch_git_experiment/GenerateAngleHistogram.java
@@ -4,6 +4,8 @@ import static uk.al_richard.experimental.angles.Util.square;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import eu.similarity.msc.core_concepts.Metric;
 import eu.similarity.msc.data.MetricSpaceResource;
@@ -37,16 +39,19 @@ public class GenerateAngleHistogram {
 	}
 
 	private MetricSpaceResource<Integer, float[]> msr;
-	Map<Integer, float[]> data;
-	Map<Integer, float[]> queries;
-	Metric<float[]> metric;
-	final Map<Integer, Integer[]> nnIds;
+	private Map<Integer, float[]> data;
+	private Map<Integer, float[]> queries;
+	private Metric<float[]> metric;
+	private Map<Integer, Integer[]> nnIds;
 	private Map<Integer, double[]> thresholds;
-	private float[] viewpoint;
+	private Set<Integer> viewpointIds;
 	private Iterator<Integer> dataSetIdIterator;
 	private final String dataset_name;
+	private int noOfSampledPoints;
+	private int noOfQueries;
 
-	public GenerateAngleHistogram(MetricSpaceResource<Integer, float[]> msr) throws Exception {
+	public GenerateAngleHistogram(MetricSpaceResource<Integer, float[]> msr, int noOfQueries, int noOfViewpoints,
+			int noOfSampledPoints) throws Exception {
 		this.msr = msr;
 		this.data = msr.getData();
 		this.queries = msr.getQueries();
@@ -55,9 +60,12 @@ public class GenerateAngleHistogram {
 		this.thresholds = msr.getThresholds();
 		this.dataset_name = msr.getClass().getName();
 		this.dataSetIdIterator = this.data.keySet().iterator();
-		// make viewpoint 2nd data point...!
-		this.data.get(this.dataSetIdIterator.next());
-		this.viewpoint = this.data.get(this.dataSetIdIterator.next());
+		this.viewpointIds = new TreeSet<>();
+		for (int i = 0; i < noOfViewpoints; i++) {
+			this.viewpointIds.add(this.dataSetIdIterator.next());
+		}
+		this.noOfSampledPoints = noOfSampledPoints;
+		this.noOfQueries = noOfQueries;
 	}
 
 	/**
@@ -69,49 +77,55 @@ public class GenerateAngleHistogram {
 	public void generateAngles(boolean constrained) {
 		System.out.println(this.dataset_name + " " + constrained);
 
+		int noOfQueriesToTest = this.noOfQueries;
 		// randomly selected query, in fact first element of queries
 		for (int queryId : this.queries.keySet()) {
+			if (noOfQueriesToTest > 0) {
+				// randomly selected viewpoint, in fact first element of data
+				float[] query = this.queries.get(queryId);
+				Integer[] nnids = this.nnIds.get(queryId);
 
-			// randomly selected viewpoint, in fact first element of data
-			float[] query = this.queries.get(queryId);
-			Integer[] nnids = this.nnIds.get(queryId);
-
-			AngleInfo ai = getAngleSample(constrained, this.viewpoint, query, nnids);
-			System.out.println(
-					queryId + "\t" + this.thresholds.get(queryId)[5] + "\t" + ai.getMean() + "\t" + ai.getStd());
+				AngleInfo ai = getAngleSample(constrained, query, nnids);
+				System.out.println(
+						queryId + "\t" + this.thresholds.get(queryId)[5] + "\t" + ai.getMean() + "\t" + ai.getStd());
+				noOfQueriesToTest--;
+			}
 		}
 	}
 
 	@SuppressWarnings("boxing")
-	private AngleInfo getAngleSample(boolean constrained, float[] a, float[] b, Integer[] nnids) {
-		// run through the query nearest neighbours, avoid the first as it may be the
-		// query itself in some cases
-		boolean first = true;
-		// let's just do 100 nns max
-		int noToDo = 100;
-		double[] angles = new double[nnids.length - 1];
-		int ptr = 0;
+	private AngleInfo getAngleSample(boolean constrained, float[] b, Integer[] nnids) {
+		int sampledPoints = Math.min(this.noOfSampledPoints, nnids.length - 1);
+		double[] angles = new double[this.viewpointIds.size() * sampledPoints];
 		double acc = 0;
-		for (int nnid : nnids) {
-			if (noToDo > 0) {
-				if (first) {
-					first = false;
-				} else {
-					float[] c;
-					if (constrained) {
-						c = this.data.get(nnid);
+		int ptr = 0;
+		for (int viewPointId : this.viewpointIds) {
+			float[] a = this.data.get(viewPointId);
+			// run through the query nearest neighbours, avoid the first as it may be the
+			// query itself in some cases
+			boolean first = true;
+			int noToDo = this.noOfSampledPoints;
+			for (int nnid : nnids) {
+				if (noToDo > 0) {
+					if (first) {
+						first = false;
 					} else {
-						c = getRandomDatum();
+						float[] c;
+						if (constrained) {
+							c = this.data.get(nnid);
+						} else {
+							c = getRandomDatum();
+						}
+
+						double aA = this.metric.distance(b, c);
+						double bB = this.metric.distance(a, c);
+						double cC = this.metric.distance(a, b);
+
+						double theta = Math.acos((square(aA) + square(cC) - square(bB)) / (2 * aA * cC));
+						angles[ptr++] = theta;
+						acc += theta;
+						noToDo--;
 					}
-
-					double aA = this.metric.distance(b, c);
-					double bB = this.metric.distance(a, c);
-					double cC = this.metric.distance(a, b);
-
-					double theta = Math.acos((square(aA) + square(cC) - square(bB)) / (2 * aA * cC));
-					angles[ptr++] = theta;
-					acc += theta;
-					noToDo--;
 				}
 			}
 		}
